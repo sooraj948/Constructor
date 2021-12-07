0afd3b82604a9f37449192d117a0a08cba383c3f
Vivek Tangudu <77504154+vivektangudu123@users.noreply.github.com>
2021-12-07 10:08:21 +0530

    Update block.cpp

--- block.cpp
+++ block.cpp
@@ -12,9 +12,9 @@ Block::Block(const char* texturesheet, SDL_Renderer* ren, int x, int y) :
 	;
 }
 
-int Block::update(bool fall)
+float Block::update(bool fall)
 {
-	//xpos = 0; ypos = 0;
+
 	srcrect.h = 16;
 	srcrect.w = 16;
 	srcrect.x = 0;
@@ -22,12 +22,10 @@ int Block::update(bool fall)
 	destrect.x = xpos;
 	destrect.y = ypos;
 
-	/*destrect.h = srcrect.h;
-	destrect.w =  srcrect.w;*/
-	destrect.h = 100;
-	destrect.w = 100;
-	/*xpos = xpos + 1;
-	ypos = pow(((xpos-500)(xpos-500)),0.5);*/
+
+	destrect.h = 90;
+	destrect.w = 90;
+	float ha;
 	if (!fall)
 	{
 		if (dire == 0)
@@ -44,17 +42,12 @@ int Block::update(bool fall)
 		if (xpos < 410)
 		{
 			dire = 0;
-		}/*
-		if (xpos <= 500)
-		{
-			ypos = pow(((xpos - 410) * (xpos - 410)), 0.5);
 		}
-		else {
-			ypos = pow(((590 - xpos) * (590 - xpos)), 0.5);
-		}*/
-		int an = xpos - 410;
+
+		int an = abs(xpos - 500);
 		double x = an * 3.14159 / 180;
-		ypos = 90 * sin(x);
+		ypos = 90 * cos(x);
+	    ha = 90 * cos(x);
 
 
 	}
@@ -64,8 +57,29 @@ int Block::update(bool fall)
 		ypos=ypos+0.1*time_after_press;
 		time_after_press++;
 	}
-	
-	return ypos;
+	float ang;
+	if (dire == 0)
+	{
+		if (xpos < 500)
+		{
+			ang = ha;
+		}
+		else {
+			ang = 180 - ha;
+		}
+	}
+	else
+	{
+		if (xpos > 500)
+		{
+			ang = 180 - ha;
+		}
+		else {
+			ang = ha;
+		}
+	}
+	//cout << ang << " ";
+	return ang;
 
 }
 
