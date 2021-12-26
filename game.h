4ab332b7ca96a4ec8194be045fed783d450bf55c
Vivek Tangudu <77504154+vivektangudu123@users.noreply.github.com>
2021-12-26 16:12:03 +0530

    Update game.h

--- game.h
+++ game.h
@@ -18,19 +18,20 @@ public:
 	void update();
 	void render();
 	bool running() { return isrunning; }
-	bool init(const char * title);
+	bool init(const char* title);
 	int getcount() { return count; }
 	static SDL_Event e;
 	static bool landed;
+
 	//bool static fall;
 	//static vector <GameObject*> landed_blocks;
 
 private:
-	bool isrunning=true,fall=false;
+	bool isrunning = true, fall = false;
 	SDL_Window* window;
 	SDL_Surface* gScreenSurface = NULL;
 	SDL_Renderer* renderer;
 	const int SCREEN_WIDTH = 1024;
 	const int SCREEN_HEIGHT = 640;
-	int count = 0;
+	int count = 0,lives=0;
 };
