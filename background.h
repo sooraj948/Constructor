d6443cc307449546d2cb5c8361d195081b710da8
Vivek Tangudu <77504154+vivektangudu123@users.noreply.github.com>
2021-12-30 14:54:35 +0530

    Update background.h

--- background.h
+++ background.h
@@ -5,10 +5,12 @@
 class Background : public GameObject
 {
 private:
+	int a=0;
 	
 
 public:
 	Background(const char* texturesheet, SDL_Renderer* ren, int x, int y);
 	void update();
+	void godown(int y);
 
 };
