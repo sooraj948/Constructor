3edeebd99a081764919bbb5c9210c17275fe0f47
Vivek Tangudu <77504154+vivektangudu123@users.noreply.github.com>
2022-01-02 10:55:36 +0530

    Update rope.h

--- rope.h
+++ rope.h
@@ -2,7 +2,24 @@
 #include "gameobject.h"
 
 
+class Rope : public GameObject#pragma once
+#include "gameobject.h"
+
+
 class Rope : public GameObject
+{
+private:
+	int dire = 0;//for direction
+	int time_after_press = 0;
+protected:
+	int ang = 0;
+
+public:
+	Rope(const char* texturesheet, SDL_Renderer* ren, int x, int y);
+	void update(float ypos,int v);
+	void render();
+};
+
 {
 private:
 	int dire = 0;//for direction
